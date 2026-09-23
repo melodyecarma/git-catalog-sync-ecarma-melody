f61cfd4915c00bc037b39de22e6243040cc1f13d
Melody Ness Ecarma <ecarmamelody@gmail.com>
2026-09-23 22:07:45 +0800

    Add 20 dollar maximum fee cap - ecarma.melody

--- catalog.js
+++ catalog.js
@@ -3,8 +3,8 @@ function isValidLoan(daysLate) {
 }
 
 function calculateLateFee(daysLate, ratePerDay) {
-  if (daysLate <= 1) return 0;
-  return Math.floor(daysLate * ratePerDay);
+  const fee = Math.floor(daysLate * ratePerDay);
+  return Math.min(fee, 20);
 }
 
 module.exports = { isValidLoan, calculateLateFee };
